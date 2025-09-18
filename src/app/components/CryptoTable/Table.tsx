c4344d8705463b7bf00c6faa92bca7c47b21c884
Ahmad Reza <ahmadrezabarfarazi@gmail.com>
2025-09-18 17:30:01 +0330

    show more

--- src/app/components/CryptoTable/Table.tsx
+++ src/app/components/CryptoTable/Table.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
 import { openDB } from "idb";
 import { CryptoCurrencyList, getCryptocurrency, Quote } from "@/app/http/crypto";
@@ -103,7 +102,10 @@ export async function saveCryptoCurrency(cryptoCurrency: CryptoCurrencyList[]) {
 
 function Table(){
     const [data, setData] = useState<CryptoCurrencyList[]>([]);
+    const [showLimited, setShowLimited] = useState(true);
 	const [loading, setLoading] = useState(true);
+
+    const displayedData = showLimited ? data.slice(0, 10) : data;
 
     useEffect(() => {
         async function fetchData() {
@@ -130,18 +132,28 @@ function Table(){
     }, []);
 
 	return (
-		<DataTable
-			columns={columns}
-			data={data}
-            pagination
-			progressPending={loading}
-            progressComponent={
-                <div className="p-6 text-center">
-                  <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-blue-500 mx-auto mb-2"></div>
-                  <p>loading...</p>
-                </div>
-              }
-		/>
+        <Fragment>
+            <DataTable
+                columns={columns}
+                data={displayedData}
+                pagination={!showLimited}
+                progressPending={loading}
+                paginationPerPage={50}
+                paginationRowsPerPageOptions={[50, 100]}
+                progressComponent={
+                    <div className="p-6 text-center">
+                        <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-blue-500 mx-auto mb-2"></div>
+                        <p>loading...</p>
+                    </div>
+                }
+            />
+            {(!loading && showLimited) && (
+                <button
+                    onClick={() => setShowLimited(prev => !prev)}
+                    className="px-4 py-2 bg-blue-500 text-white rounded mx-auto block cursor-pointer my-5"
+                >show more</button>
+            )}
+        </Fragment>
 	);
 }
 
